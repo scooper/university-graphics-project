5767b241151cd93f1210704339043039b1ca7cf5
Samuel Cooper <ll15s3c@leeds.ac.uk>
2018-12-14 16:37:42 +0000

    needed textures, model class set up and gitignore updated

--- inc/Model.h
+++ inc/Model.h
@@ -3,6 +3,25 @@
 
 #include <GL/glut.h>
 #include <GL/glu.h>
-#include <fstream>
 
+class Model {
+
+  public:
+
+  Model();
+  void loadModel();
+
+  private:
+
+  int _x;
+  int _y;
+  int _z;
+  int _width;
+  int _height;
+  int _depth;
+  GLuint _vboID;
+
+
+
+};
 #endif
--- src/Model.cpp
+++ src/Model.cpp
@@ -1,2 +1,12 @@
 #include <GL/glut.h>
 #include <GL/glu.h>
+#include <fstream>
+#include "Model.h"
+
+Model::Model() {
+  
+}
+
+Model::loadModel() {
+
+}
